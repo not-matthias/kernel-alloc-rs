6a3533296521949aa87a24b6b198dea055ef3746
not-matthias <26800596+not-matthias@users.noreply.github.com>
2020-08-16 19:52:52 +0200

    Added the alloc error handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![doc(include = "../README.md")]
-#![feature(external_doc)]
 #![no_std]
+#![feature(external_doc)]
+#![feature(alloc_error_handler)]
 
 use crate::nt::{ExAllocatePool, ExFreePool, PoolType};
 use core::alloc::{GlobalAlloc, Layout};
@@ -22,4 +23,9 @@ unsafe impl GlobalAlloc for KernelAlloc {
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) { ExFreePool(ptr as _); }
+}
+
+#[alloc_error_handler]
+fn alloc_error(layout: Layout) -> ! {
+    panic!("{:?} alloc memory error", layout);
 }
