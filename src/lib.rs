1347d1a801cfdff05998cbdf10b7b5daad9584b0
not-matthias <26800596+not-matthias@users.noreply.github.com>
2020-08-21 21:38:58 +0200

    Updated the documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
-#![doc(include = "../README.md")]
+//! Rust has many useful abstractions and utils that require heap allocations.
+//! `String`, `Vec` and `Box` are some of them. To be able to use them, we need
+//! to allocate memory at runtime, which requires a custom allocator.
+//!
+//! If you want to find out more about it, please refer to the [alloc::GlobalAllocator](https://doc.rust-lang.org/std/alloc/trait.GlobalAlloc.html) and the [Rust book](https://doc.rust-lang.org/edition-guide/rust-2018/platform-and-target-support/global-allocators.html).
+//!
+//! ## Example
+//!
+//! Add the following to your code to define new global allocator:
+//!
+//! ```rust
+//! use kernel_alloc::KernelAlloc;
+//!
+//! #[global_allocator]
+//! static GLOBAL: KernelAlloc = KernelAlloc;
+//! ```
+
 #![no_std]
-#![feature(external_doc)]
 #![feature(alloc_error_handler)]
 
 use crate::nt::{ExAllocatePool, ExFreePool, PoolType};
